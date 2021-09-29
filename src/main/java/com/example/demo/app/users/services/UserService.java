9d51bdc5d095bbdd8aa83bb3417d6a2e80372f55
richizero <richizero@hotmail.com>
2021-09-29 17:26:12 -0600

    se agrega metodo para actualizar usuario

--- src/main/java/com/example/demo/app/users/services/UserService.java
+++ src/main/java/com/example/demo/app/users/services/UserService.java
@@ -12,13 +12,35 @@ public class UserService
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
+
         this.userRepository = userRepository;
     }
+
+    public User findById(Integer id)
+    {
+        return userRepository.findById(id).get();
+    }
+
     public List<User>findALL(){
+
         return userRepository.findAll();
     }
 
     public User create(User user){
+
         return userRepository.save(user);
+    }
+    public User update(Integer id, User user) throws Exception
+    {
+        User foundUser = userRepository.findById(id).get();
+        if (user != null)
+        userRepository.save(user);
+        {
+            return userRepository.save(user);
+        }
+        else
+        {
+            throw now Exception("usuario no encontrado");
+        }
     }
 }
