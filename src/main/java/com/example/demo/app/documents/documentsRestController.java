ec5d0f1509311b3173640aadde7cfab10701e3dd
richizero <richizero@hotmail.com>
2021-10-11 18:45:43 -0600

    se agrega el filemanager

--- src/main/java/com/example/demo/app/documents/documentsRestController.java
+++ src/main/java/com/example/demo/app/documents/documentsRestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.app.documents;
 
+import com.example.demo.components.FileManager;
 import com.example.demo.utils.Router;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(Router.DOCUMENTS)
@@ -17,11 +19,20 @@ public class documentsRestController {
     @Value("${bucket}")
     private String bucket;
 
+    private FileManager fileManager;
+
+    public documentsRestController(FileManager fileManager) {
+        this.fileManager = fileManager;
+    }
+
 
     @PostMapping("upload")
     public void upload(@RequestParam("file") MultipartFile multipartfile,
                        @RequestParam("name") String name){
-        // NOMBRE DEL ARCHIVO
-        System.out.println(bucket + File.separator + multipartfile.getOriginalFilename());
+        try {
+            fileManager.upload(multipartfile,name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/demo/components/FileManager.java
+++ src/main/java/com/example/demo/components/FileManager.java
@@ -1,6 +1,12 @@
 package com.example.demo.components;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 /**
  * componente para subir y descargar archivos
@@ -10,7 +16,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class FileManager {
 
-
-
-
+    /**
+     *
+     * @param multipartFile
+     * @param name
+     * @return
+     */
+    public String upload(MultipartFile multipartFile, String name) throws IOException {
+    File newFile = new File(name);
+        Files.copy(multipartFile.getInputStream(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        return name;
+    }
 }
