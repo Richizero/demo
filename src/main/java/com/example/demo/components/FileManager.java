20578e7bd33505ec7d260d7f26d1ed751ceb8444
richizero <richizero@hotmail.com>
2021-10-13 17:35:42 -0600

    se agrega el filemanager mas cosas

--- src/main/java/com/example/demo/components/FileManager.java
+++ src/main/java/com/example/demo/components/FileManager.java
@@ -1,11 +1,17 @@
 package com.example.demo.components;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import org.springframework.core.io.Resource;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 /**
@@ -16,6 +22,9 @@ import java.nio.file.StandardCopyOption;
 @Component
 public class FileManager {
 
+    @Value("${bucket}")
+    private String bucket;
+
     /**
      *
      * @param multipartFile
@@ -23,8 +32,14 @@ public class FileManager {
      * @return
      */
     public String upload(MultipartFile multipartFile, String name) throws IOException {
-    File newFile = new File(name);
+    File newFile = new File(bucket + File.separator + multipartFile.getOriginalFilename());
         Files.copy(multipartFile.getInputStream(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        return name;
+
+        return newFile.getName();
+    }
+
+    public Resource download(String fullPathFileName) throws MalformedURLException {
+        Path path = Paths.get(fullPathFileName).normalize();
+        return new UrlResource(path.toUri());
     }
 }
