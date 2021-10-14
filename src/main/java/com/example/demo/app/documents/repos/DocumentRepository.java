09cb558a0b5151ac25e40eb6ac1c54cfe060d489
richizero <richizero@hotmail.com>
2021-10-13 18:11:27 -0600

    se agrega documentrepository

---
+++ src/main/java/com/example/demo/app/documents/repos/DocumentRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.app.documents.repos;
+
+import com.example.demo.app.documents.models.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class DocumentRepository {
+}
